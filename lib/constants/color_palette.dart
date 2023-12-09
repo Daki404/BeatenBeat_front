a75e086cf0c08c03b58a94b6a5e6d2ae4419097b
daki404 <daki@403.1@gmail.com>
2023-12-10 02:12:35 +0900

    feat(constants/color_palette): add color_palatte
    임시로 사용할 컬러팔레트를 추가 하였습니다.
    
    ref: #1

---
+++ lib/constants/color_palette.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class ColorPalette {
+  ColorPalette._();
+  static const Color navy = Color.fromARGB(255, 39, 0, 93);
+  static const Color purple = Color.fromARGB(255, 148, 0, 255);
+  static const Color blue = Color.fromARGB(255, 174, 210, 255);
+  static const Color sky = Color.fromARGB(255, 228, 241, 255);
+  static const Color gray300 = Color(0xFFC5C8CE);
+  static const Color gray400 = Color(0xFF646F7C);
+  static const Color gray500 = Color(0xFF374553);
+  static const Color gray600 = Color(0xFF28323C);
+}
