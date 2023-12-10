3c943e3fd12b4136bfae1e4a7ba9c9dddb7d2f63
Jangjun <daki403.1@gmail.com>
2023-12-10 21:30:40 +0900

    feat(routes): add routes and off animation
    main.dart에 있었던 route를,
    routes 폴더를 만들어 분리하였습니다.
    또한, route시에 default animation이 동작하므로,
    duration을 0으로 주어 동작하지 않게 했습니다.
    
    ref: #1

---
+++ lib/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const String routeHome = '/';
+const String routeLogin = '/login';
+const String routeAccount = '/account';
+const String routeChannel = '/channel';
+
+final navKey = new GlobalKey<NavigatorState>();
