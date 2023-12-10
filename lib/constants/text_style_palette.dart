3c4d7d1936f37e448570865b1295148949b84e0f
Jangjun <daki403.1@gmail.com>
2023-12-10 21:34:51 +0900

    feat(constants/text_style_palette) : add text_style_palette
    일관성 있는 TextStyle 사용을 위해
    constants에 text_style_palette를 추가 하였습니다
    
    ref: #1

---
+++ lib/constants/text_style_palette.dart
@@ -0,0 +1,11 @@
+import 'package:beaten_beat/constants/color_palette.dart';
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  TextStyles._();
+  static const TextStyle navigation = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    color: ColorPalette.sky,
+  );
+}
