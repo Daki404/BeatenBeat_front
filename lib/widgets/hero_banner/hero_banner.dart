05ffe96fabf21d1c5f6ace9c3c248ed66c1e4537
Jangjun <daki403.1@gmail.com>
2023-12-15 21:07:46 +0900

    feat(hero_banner): add repeatY in image
    화면의 가로와 세로크기 만큼 영역을 할당하고,
    Y축으로 반복을 진행하도록 변경 하였습니다.
    
    ref: #1

--- lib/widgets/hero_banner/hero_banner.dart
+++ lib/widgets/hero_banner/hero_banner.dart
@@ -1,4 +1,3 @@
-import 'package:beaten_beat/constants/color_palette.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,28 +7,18 @@ class HeroBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height - 100;
 
-    return Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
-          Spacer(),
-          Container(
-              width: screenWidth * 0.5,
-              height: screenWidth * 0.5 * 9 / 16,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage('assets/images/lp_album2.png'),
-                fit: BoxFit.contain,
-              ))),
-          Container(
-              width: screenWidth * 0.3,
-              height: screenWidth * 0.3,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage('assets/logos/logo_text.png'),
-                fit: BoxFit.contain,
-              ))),
-          Spacer(),
-        ]));
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 100),
+      width: screenWidth,
+      height: screenHeight,
+      child: Positioned.fill(
+        child: Image.asset(
+          'assets/logos/img_logo.png',
+          repeat: ImageRepeat.repeatY,
+        ),
+      ),
+    );
   }
 }
