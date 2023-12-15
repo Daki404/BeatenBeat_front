51256f71aa8c620f9fbf9270b230de9beb315294
Jangjun <daki403.1@gmail.com>
2023-12-15 22:57:28 +0900

    chore(assets, constants): rename assets, add style
    assets의 이름을 바꾸고, 하나 추가 하였습니다.
    text_style_palette를 추가하였습니다.
    
    ref: #1

--- lib/constants/text_style_palette.dart
+++ lib/constants/text_style_palette.dart
@@ -1,4 +1,5 @@
 import 'package:beaten_beat/constants/color_palette.dart';
+import 'package:beaten_beat/screens/welcome_page/intro_text/intro_text.dart';
 import 'package:flutter/material.dart';
 
 class TextStyles {
@@ -7,5 +8,9 @@ class TextStyles {
     fontSize: 20,
     fontWeight: FontWeight.normal,
     color: ColorPalette.sky,
+  );
+  static const TextStyle introText = TextStyle(
+    color: ColorPalette.sky,
+    fontSize: 50,
   );
 }
