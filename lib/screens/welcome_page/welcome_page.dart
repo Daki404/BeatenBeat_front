ab4594ebb00c3cd8645e872f9bda5ab051326257
Jangjun <daki403.1@gmail.com>
2023-12-18 03:37:36 +0900

    feat(welcome_page): add lp_explain
    offsetBuilder에서 offset이 변경될 떄 마다 동작하므로,
    여기서 lp_explain의 increaseDegree를 호출 합니다.
    비권장 방식이긴 하지만, GlobalKey를 생성하고 넘겼습니다.
    그리고 Key를 이용해 강제로 부모에서 자식의 increaseDegree를 호출합니다.
    
    ref: #1

--- lib/screens/welcome_page/welcome_page.dart
+++ lib/screens/welcome_page/welcome_page.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:beaten_beat/screens/welcome_page/explain_view/explain_one.dart';
+import 'package:beaten_beat/screens/welcome_page/explain_view/lp_explain.dart';
 import 'package:beaten_beat/screens/welcome_page/hero_banner/hero_banner.dart';
 import 'package:beaten_beat/screens/welcome_page/intro_view/intro_view.dart';
 import 'package:flutter/material.dart';
@@ -18,13 +18,45 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage> {
   ScrollController _scrollController = ScrollController();
 
+  late LpExplain lpOne;
+  late LpExplain lpTwo;
+  late LpExplain lpThree;
+
+  GlobalKey<LpExplainState> lpOneKey = GlobalKey();
+  GlobalKey<LpExplainState> lpTwoKey = GlobalKey();
+  GlobalKey<LpExplainState> lpThreeKey = GlobalKey();
+
   @override
   void initState() {
+    super.initState();
     _scrollController.addListener(() {
-      print(_scrollController.offset);
+      print('offset = ${_scrollController.offset}');
     });
 
-    super.initState();
+    lpOne = LpExplain(
+      key: lpOneKey,
+      title: 'YouTube 컨텐츠 탐색',
+      script:
+          '리모컨 (Multiple Remote Controller) 으로 멤버와 함께 YouTube 컨텐츠를 탐색하고, 플레이리스트에 추가해 보세요.',
+      videoUrl:
+          'https://bgms-bucket.s3.ap-northeast-2.amazonaws.com/video/step1.mp4',
+    );
+    lpTwo = LpExplain(
+      key: lpTwoKey,
+      title: '리모컨과 연결된 재생 플레이어',
+      script:
+          'YouTube 컨텐츠 플레이어를 실행하여 플레이리스트를 재생할 수 있습니다. 플레이어를 켜고 스피커에 연결하여 공간에 음악을 재생해 보세요.',
+      videoUrl:
+          'https://bgms-bucket.s3.ap-northeast-2.amazonaws.com/video/step1.mp4',
+    );
+    lpThree = LpExplain(
+      key: lpThreeKey,
+      title: '원격 재생 컨트롤 기능',
+      script:
+          '리모컨이 설치 되어있는 모든 Device에서 컨텐츠 재생, 컨텐츠 넘기기, 볼륨 조절 등 YouTube 컨텐츠 플레이어를 제어해 보세요!',
+      videoUrl:
+          'https://bgms-bucket.s3.ap-northeast-2.amazonaws.com/video/step1.mp4',
+    );
   }
 
   @override
@@ -54,42 +86,33 @@ class _WelcomePageState extends State<WelcomePage> {
             },
             offsetBuilder: (scrollOffset) => Offset(0, 0),
           ),
-          ScrollTransformItem(
-            builder: (scrollOffset) {
-              return Container(
-                color: ColorPalette.paua,
-                height: screenHeight,
-              );
-            },
-            offsetBuilder: (scrollOffset) => Offset(
+          ScrollTransformItem(builder: (scrollOffset) {
+            return lpOne;
+          }, offsetBuilder: (scrollOffset) {
+            lpOneKey.currentState?.increaseDegree(1);
+            return Offset(
                 2 * screenWidth - scrollOffset * screenWidth / screenHeight,
-                max(0, scrollOffset - 2 * screenHeight)),
-          ),
-          ScrollTransformItem(
-            builder: (scrollOffset) {
-              return Container(
-                color: ColorPalette.kingfisherDaisy,
-                height: screenHeight,
-              );
-            },
-            offsetBuilder: (scrollOffset) => Offset(
+                max(0, scrollOffset - 2 * screenHeight));
+          }),
+          ScrollTransformItem(builder: (scrollOffset) {
+            return lpTwo;
+          }, offsetBuilder: (scrollOffset) {
+            lpTwoKey.currentState?.increaseDegree(-1);
+            return Offset(
                 3 * screenWidth - scrollOffset * screenWidth / screenHeight,
-                max(-screenHeight, scrollOffset - 3 * screenHeight)),
-          ),
-          ScrollTransformItem(
-            builder: (scrollOffset) {
-              return Container(
-                color: ColorPalette.darkBlue,
-                height: screenHeight,
-              );
-            },
-            offsetBuilder: (scrollOffset) => Offset(
+                max(-screenHeight, scrollOffset - 3 * screenHeight));
+          }),
+          ScrollTransformItem(builder: (scrollOffset) {
+            return lpThree;
+          }, offsetBuilder: (scrollOffset) {
+            lpThreeKey.currentState?.increaseDegree(1);
+            return Offset(
                 max(
                     0,
                     4 * screenWidth -
                         scrollOffset * screenWidth / screenHeight),
-                max(-screenHeight, scrollOffset - 4 * screenHeight)),
-          ),
+                max(-screenHeight, scrollOffset - 4 * screenHeight));
+          }),
           ScrollTransformItem(
             builder: (scrollOffset) {
               return Container(
