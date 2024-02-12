72f7dd2cc3716bb370dae2653d2b592009283290
Jangjun <daki403.1@gmail.com>
2024-02-12 02:56:08 +0000

    feat(login) : add oauth2.0 page and test
    Naver, google, Kakako의 Oauth2를 local-host 상에서
    테스트 하는 코드를 추가 하였습니다.
    
    account_page에서 로그인이 가능하며,
    channel_page에서 request 테스트를 콘솔로 확인가능
    
    ref: #2

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,6 +1,7 @@
 class Url {
   Url._();
-  static const String kakaoOAuthUrl = 'https://naver.com';
-  static const String naverOAuthUrl = 'https://naver.com';
-  static const String googleOAuthUrl = 'https://naver.com';
+  static const String loginOAuthUrl = 'http://localhost:8080/api/v1/login';
+  static const String kakaoOAuthUrl = loginOAuthUrl + '/kakao';
+  static const String naverOAuthUrl = loginOAuthUrl + '/naver';
+  static const String googleOAuthUrl = loginOAuthUrl + '/google';
 }
